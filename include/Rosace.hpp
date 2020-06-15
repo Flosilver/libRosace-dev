f7c6e278b8d6594d1b166a784e66b0245607fc0c
Flosilver <florentin.b98@gmail.com>
2020-06-15 18:07:03 +0200

    update Rosace.hpp

--- include/Rosace.hpp
+++ include/Rosace.hpp
@@ -7,5 +7,11 @@
 #include "Card.hpp"
 #include "Deck.hpp"
 #include "IMovable.hpp"
+#include "Player.hpp"
+#include "Pawn.hpp"
+#include "Game.hpp"
 
+namespace rsc{
+typedef enum{North, East, South, West} player_t;     // [0,1,2,3]  number of the player according to it's position
+}
 #endif
