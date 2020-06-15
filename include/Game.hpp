ef68d779ac3a481b88eafe3daf41f1a3813c4a09
Pierre Schneider <pierrebert.schneider@gmail.com>
2020-06-15 17:55:06 +0200

    add game.hpp

---
+++ include/Game.hpp
@@ -0,0 +1,38 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "Player.hpp"
+#include <list>
+
+namespace rsc
+{
+class Game
+{
+    protected:
+        std::vector<Player*> players(4);    // list of 4 players
+        Deck deck;                          // deck
+        Deck discard;                       // a deck were we put used cards
+        // abstract field for different types of map
+
+
+    public:
+        int state;                          // game's state
+
+
+        /* Operateurs */
+
+        /* accesseur */
+        const int& getState() const {return state;}
+        const Deck& getDeck() const {return deck;}
+        const Deck& getDiscard() const {return discard;}
+        const Player& getPlayer(int id) const {return &players[id];}
+
+        void setState(const int s) {state = s;}
+
+        /* méthode */
+
+};
+
+} // namespace rsc
+
+#endif
